eb8cd03a28b5ec486d54b5f7ab44cdcab05b5820
Adam Caldwell <lupusrex@gmail.com>
2014-11-01 12:19:29 -0700

    updated for assignment 2

---
+++ F2-mainproject/F2-mainproject/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  F2-mainproject
+//
+//  Created by Adam Caldwell on 11/1/14.
+//  Copyright (c) 2014 Adam Caldwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var myPerson = Person(firstName: "Adam", lastName: "Caldwell", isStudent: true)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func printName(sender: UIButton) {
+        println(myPerson.returnFullName())
+    }
+
+
+}
+
---
+++ F2-mainproject/PersonClass.swift
@@ -0,0 +1,27 @@
+//
+//  PersonClass.swift
+//  F2-mainproject
+//
+//  Created by Adam Caldwell on 11/1/14.
+//  Copyright (c) 2014 Adam Caldwell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Person {
+    var firstName : String
+    var lastName : String
+    var isStudent : Bool
+    
+    init (firstName : String, lastName : String, isStudent: Bool) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.isStudent = isStudent
+    }
+    
+    func returnFullName () -> String {
+        var fullName : String = self.firstName + " " + self.lastName
+        return fullName
+    }
+}
