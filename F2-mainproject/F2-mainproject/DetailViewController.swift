4dc2dcd890dfc7c17980ec043f33d8247cbaf005
Adam Caldwell <lupusrex@gmail.com>
2014-11-09 13:02:16 -0800

    updated for assignment 4

---
+++ F2-mainproject/F2-mainproject/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  F2-mainproject
+//
+//  Created by Adam Caldwell on 11/9/14.
+//  Copyright (c) 2014 Adam Caldwell. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
